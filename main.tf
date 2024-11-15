5bb9a7dcd204f683ac73c6111b331b5fa259d2f6
geethak5513 <41056499+geethak5513@users.noreply.github.com>
2024-11-16 02:17:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_resource_group" "rg1" {
+  name     = "myresource_group"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "vn1" {
+  name                = "myvirtual_network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+}
+
+resource "azurerm_subnet" "sbn1" {
+  name                 = "mysubnet"
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.vn1.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+}
