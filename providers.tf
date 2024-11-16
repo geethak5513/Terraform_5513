10047ea961e3f5a4a226f4a338a467c860883a29
geethak5513 <41056499+geethak5513@users.noreply.github.com>
2024-11-16 09:34:28 +0530

    Update providers.tf
    authentication

--- providers.tf
+++ providers.tf
@@ -7,6 +7,9 @@ terraform {
   }
 }
 provider "azurerm" {
-  resource_provider_registrations = "none"
   features {}
+  client_id       = "00000000-0000-0000-0000-000000000000"
+  client_secret   = var.client_secret
+  tenant_id       = "9a8cd433-6113-49e5-aa7f-42788a01a246"
+  subscription_id = "20000000-0000-0000-0000-000000000000"
 }
